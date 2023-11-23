bdc34107afe3c4b1a76a2d66b6542bb823d3a321
bwhittaker69 <brett@whittaker.ca>
2023-11-23 11:57:11 -0500

    updates

--- ca.whitaker.Graphics.BulkImageResize/Program.cs
+++ ca.whitaker.Graphics.BulkImageResize/Program.cs
@@ -8,19 +8,16 @@ class Program
     static void Main(string[] args)
     {
         string sourceDirectory = @"E:\source\repos\ca.whitaker.Graphics\Solution Items";
-        string targetDirectory = @"E:\source\repos\ca.whitaker.Graphics\Solution Items\Social Media";
+        string targetDirectory = @"E:\source\repos\ca.whitaker.Graphics\Solution Items\Output";
 
-        ResizeImages(sourceDirectory, targetDirectory, 12);
-        ResizeImages(sourceDirectory, targetDirectory, 24);
-        ResizeImages(sourceDirectory, targetDirectory, 36);
-        ResizeImages(sourceDirectory, targetDirectory, 48);
+        ResizeImages(sourceDirectory, targetDirectory, 12, true);
+        ResizeImages(sourceDirectory, targetDirectory, 24, true);
+        ResizeImages(sourceDirectory, targetDirectory, 36, true);
+        ResizeImages(sourceDirectory, targetDirectory, 48, true);
 
-        // Optional: Force garbage collection after processing
-        GC.Collect();
-        GC.WaitForPendingFinalizers();
     }
 
-    static void ResizeImages(string sourceDir, string targetDir, int size)
+    static void ResizeImages(string sourceDir, string targetDir, int size, bool grayscale)
     {
         if (!Directory.Exists(targetDir))
             Directory.CreateDirectory(targetDir);
@@ -36,8 +33,15 @@ class Program
                         string fileName = Path.GetFileNameWithoutExtension(file) + $"_{size}.png";
                         string savePath = Path.Combine(targetDir, fileName);
                         newImage.Save(savePath, ImageFormat.Png);
+                        using (Bitmap grayscaleImage = ConvertToGrayscale(new Bitmap(newImage)))
+                        {
+                            fileName = Path.GetFileNameWithoutExtension(fileName) + "_grayscale.png";
+                            savePath = Path.Combine(targetDir, fileName);
+                            grayscaleImage.Save(savePath, ImageFormat.Png);
+                        }
                     }
                 }
+
             }
             catch (Exception ex)
             {
@@ -45,4 +49,32 @@ class Program
             }
         }
     }
+    static Bitmap ConvertToGrayscale(Bitmap original)
+    {
+        Bitmap grayscale = new Bitmap(original.Width, original.Height);
+
+        using (Graphics g = Graphics.FromImage(grayscale))
+        {
+            // Create grayscale color matrix
+            ColorMatrix colorMatrix = new ColorMatrix(
+                new float[][]
+                {
+                    new float[] { .3f, .3f, .3f, 0, 0 },
+                    new float[] { .59f, .59f, .59f, 0, 0 },
+                    new float[] { .11f, .11f, .11f, 0, 0 },
+                    new float[] { 0, 0, 0, 1, 0 },
+                    new float[] { 0, 0, 0, 0, 1 }
+                });
+
+            using (ImageAttributes attributes = new ImageAttributes())
+            {
+                attributes.SetColorMatrix(colorMatrix);
+                g.DrawImage(original, new Rectangle(0, 0, original.Width, original.Height),
+                            0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes);
+            }
+        }
+
+        return grayscale;
+    }
+
 }
