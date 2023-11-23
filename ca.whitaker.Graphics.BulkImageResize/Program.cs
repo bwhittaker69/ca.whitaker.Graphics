3f96063b34ac6dc9a66a4fbb5e537457593fde2b
bwhittaker69 <brett@whittaker.ca>
2023-11-23 12:00:51 -0500

    added basic commets to the code

--- ca.whitaker.Graphics.BulkImageResize/Program.cs
+++ ca.whitaker.Graphics.BulkImageResize/Program.cs
@@ -7,55 +7,92 @@ class Program
 {
     static void Main(string[] args)
     {
-        string sourceDirectory = @"E:\source\repos\ca.whitaker.Graphics\Solution Items";
-        string targetDirectory = @"E:\source\repos\ca.whitaker.Graphics\Solution Items\Output";
+        // Prompt for source directory
+        Console.WriteLine("Enter source directory path:");
+        string sourceDirectory = Console.ReadLine();
 
-        ResizeImages(sourceDirectory, targetDirectory, 12, true);
-        ResizeImages(sourceDirectory, targetDirectory, 24, true);
-        ResizeImages(sourceDirectory, targetDirectory, 36, true);
-        ResizeImages(sourceDirectory, targetDirectory, 48, true);
+        // Prompt for target directory
+        Console.WriteLine("Enter target directory path:");
+        string targetDirectory = Console.ReadLine();
 
+        // Check if source directory exists
+        if (!Directory.Exists(sourceDirectory))
+        {
+            Console.WriteLine("Source directory does not exist.");
+            return;
+        }
+
+        // Check if target directory exists, create if not
+        if (!Directory.Exists(targetDirectory))
+        {
+            Console.WriteLine("Target directory does not exist. Creating it now...");
+            Directory.CreateDirectory(targetDirectory);
+        }
+
+        // Prompt for resize size
+        Console.WriteLine("Enter the size for resizing (e.g., 12, 24, 36, 48):");
+        if (!int.TryParse(Console.ReadLine(), out int size))
+        {
+            Console.WriteLine("Invalid size. Please enter a valid number.");
+            return;
+        }
+
+        // Prompt for grayscale option
+        Console.WriteLine("Apply grayscale? (yes/no)");
+        bool grayscale = Console.ReadLine().Trim().ToLower() == "yes";
+
+        // Process images
+        ResizeImages(sourceDirectory, targetDirectory, size, grayscale);
     }
 
     static void ResizeImages(string sourceDir, string targetDir, int size, bool grayscale)
     {
-        if (!Directory.Exists(targetDir))
-            Directory.CreateDirectory(targetDir);
-
+        // Iterate over each file in the source directory
         foreach (string file in Directory.GetFiles(sourceDir, "*.png"))
         {
             try
             {
+                // Load the image
                 using (Image image = Image.FromFile(file))
                 {
+                    // Resize the image
                     using (Bitmap newImage = new Bitmap(image, new Size(size, size)))
                     {
+                        // Save the resized image
                         string fileName = Path.GetFileNameWithoutExtension(file) + $"_{size}.png";
                         string savePath = Path.Combine(targetDir, fileName);
                         newImage.Save(savePath, ImageFormat.Png);
-                        using (Bitmap grayscaleImage = ConvertToGrayscale(new Bitmap(newImage)))
+
+                        // If grayscale is applied, convert and save the grayscale image
+                        if (grayscale)
                         {
-                            fileName = Path.GetFileNameWithoutExtension(fileName) + "_grayscale.png";
-                            savePath = Path.Combine(targetDir, fileName);
-                            grayscaleImage.Save(savePath, ImageFormat.Png);
+                            using (Bitmap grayscaleImage = ConvertToGrayscale(new Bitmap(newImage)))
+                            {
+                                fileName = Path.GetFileNameWithoutExtension(fileName) + "_grayscale.png";
+                                savePath = Path.Combine(targetDir, fileName);
+                                grayscaleImage.Save(savePath, ImageFormat.Png);
+                            }
                         }
                     }
                 }
-
             }
             catch (Exception ex)
             {
+                // Handle any errors during processing
                 Console.WriteLine($"Error processing file {file}: {ex.Message}");
             }
         }
     }
+
     static Bitmap ConvertToGrayscale(Bitmap original)
     {
+        // Create a new empty bitmap for the grayscale image
         Bitmap grayscale = new Bitmap(original.Width, original.Height);
 
+        // Use graphics to edit the bitmap
         using (Graphics g = Graphics.FromImage(grayscale))
         {
-            // Create grayscale color matrix
+            // Set up the color matrix for grayscale conversion
             ColorMatrix colorMatrix = new ColorMatrix(
                 new float[][]
                 {
@@ -68,13 +105,14 @@ class Program
 
             using (ImageAttributes attributes = new ImageAttributes())
             {
+                // Apply the color matrix to the original image
                 attributes.SetColorMatrix(colorMatrix);
                 g.DrawImage(original, new Rectangle(0, 0, original.Width, original.Height),
                             0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes);
             }
         }
 
+        // Return the grayscale image
         return grayscale;
     }
-
 }
