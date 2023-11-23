44c64df7c52f0f45a743c3ebe8f3284868e64186
bwhittaker69 <brett@whittaker.ca>
2023-11-23 11:49:03 -0500

    updates

--- ca.whitaker.Graphics.BulkImageResize/Program.cs
+++ ca.whitaker.Graphics.BulkImageResize/Program.cs
@@ -7,13 +7,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        string sourceDirectory = @"E:\source\repos\ca.whitaker.Graphics\ca.whitaker.Graphics.BulkImageResize";
-        string targetDirectory = @"E:\source\repos\ca.whitaker.Graphics\ca.whitaker.Graphics.BulkImageResize\Output";
+        string sourceDirectory = @"E:\source\repos\ca.whitaker.Graphics\Solution Items";
+        string targetDirectory = @"E:\source\repos\ca.whitaker.Graphics\Solution Items\Social Media";
 
         ResizeImages(sourceDirectory, targetDirectory, 12);
         ResizeImages(sourceDirectory, targetDirectory, 24);
         ResizeImages(sourceDirectory, targetDirectory, 36);
         ResizeImages(sourceDirectory, targetDirectory, 48);
+
+        // Optional: Force garbage collection after processing
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
     }
 
     static void ResizeImages(string sourceDir, string targetDir, int size)
@@ -21,16 +25,23 @@ class Program
         if (!Directory.Exists(targetDir))
             Directory.CreateDirectory(targetDir);
 
-        foreach (string file in System.IO.Directory.GetFiles(sourceDir, "*.png"))
+        foreach (string file in Directory.GetFiles(sourceDir, "*.png"))
         {
-            using (Image image = Image.FromFile(file))
+            try
             {
-                using (Bitmap newImage = new Bitmap(image, new Size(size, size)))
+                using (Image image = Image.FromFile(file))
                 {
-                    string fileName = Path.GetFileNameWithoutExtension(file) + $"_{size}.png";
-                    string savePath = Path.Combine(targetDir, fileName);
-                    newImage.Save(savePath, ImageFormat.Png);
+                    using (Bitmap newImage = new Bitmap(image, new Size(size, size)))
+                    {
+                        string fileName = Path.GetFileNameWithoutExtension(file) + $"_{size}.png";
+                        string savePath = Path.Combine(targetDir, fileName);
+                        newImage.Save(savePath, ImageFormat.Png);
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error processing file {file}: {ex.Message}");
             }
         }
     }
