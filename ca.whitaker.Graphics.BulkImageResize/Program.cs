f1e772d4b8a107946955076ae713fbfe57c1c593
bwhittaker69 <brett@whittaker.ca>
2023-11-23 11:13:44 -0500

    finalized

--- ca.whitaker.Graphics.BulkImageResize/Program.cs
+++ ca.whitaker.Graphics.BulkImageResize/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string sourceDirectory = @"..\Solution Items";
+        string targetDirectory = @"..\Solution Items\Output";
+
+        ResizeImages(sourceDirectory, targetDirectory, 12);
+        ResizeImages(sourceDirectory, targetDirectory, 24);
+        ResizeImages(sourceDirectory, targetDirectory, 36);
+    }
+
+    static void ResizeImages(string sourceDir, string targetDir, int size)
+    {
+        if (!Directory.Exists(targetDir))
+            Directory.CreateDirectory(targetDir);
+
+        foreach (string file in System.IO.Directory.GetFiles(sourceDir, "*.png"))
+        {
+            using (Image image = Image.FromFile(file))
+            {
+                using (Bitmap newImage = new Bitmap(image, new Size(size, size)))
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(file) + $"_{size}.png";
+                    string savePath = Path.Combine(targetDir, fileName);
+                    newImage.Save(savePath, ImageFormat.Png);
+                }
+            }
+        }
+    }
+}
